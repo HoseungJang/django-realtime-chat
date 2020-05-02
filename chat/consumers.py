bb21f44cd7ce8e48b587a96180a92f6fd0ed47ea
HoseungJang <hsjang.dev@gmail.com>
2020-05-02 22:03:49 +0900

    ADD: websocket consumer

---
+++ chat/consumers.py
@@ -0,0 +1,60 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add(
+            'all',
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_send(
+            'all',
+            {
+                'type': 'chat_message',
+                'data': {
+                    'type': 'disconnect',
+                    'data': {
+                        'name': self.name
+                    }
+                }
+            }
+        )
+
+        await self.channel_layer.group_discard(
+            'all',
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+
+        if data['type'] == 'connect':
+            self.name = data['data']['name']
+        
+        await self.channel_layer.group_discard(
+            'all',
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            'all',
+            {
+                'type': 'chat_message',
+                'data': data
+            }
+        )
+
+        
+        await self.channel_layer.group_add(
+            'all',
+            self.channel_name
+        )
+
+    async def chat_message(self, event):
+        await self.send(
+            text_data=json.dumps(event['data'])
+        )
