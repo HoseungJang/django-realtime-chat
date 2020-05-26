2f8c3b1e9c227dc69084c78b16ef594c5781f6e3
HoseungJang <hsjang.dev@gmail.com>
2020-05-26 19:22:39 +0900

    ADD: duplicated name handler

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+names = []
+
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.group_add(
-            'all',
-            self.channel_name
-        )
-
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -29,11 +27,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        names.pop(names.index(self.name))
+
     async def receive(self, text_data):
         data = json.loads(text_data)
 
         if data['type'] == 'connect':
             self.name = data['data']['name']
+            names.append(self.name)
+            await self.channel_layer.group_add(
+                'all',
+                self.channel_name
+            )
+        elif data['type'] == 'names':
+            return await self.send(text_data=json.dumps({
+                'type': 'names',
+                'data': names
+            }))
 
         await self.channel_layer.group_send(
             'all',
@@ -49,7 +59,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         if data['type'] != 'disconnect' and self.channel_name == event['senderChannel']:
             return
-        
+
         await self.send(
             text_data=json.dumps(event['data'])
         )
